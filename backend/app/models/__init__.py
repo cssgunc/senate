1ff4c849339a4c33b9f21d4562ec23fa125497c0
ggreat317 <ggreat317@gmail.com>
2026-02-24 12:22:51 -0500

    rebase

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,10 +1,7 @@
 """SQLAlchemy models"""
 
-<<<<<<< HEAD
 from app.models.cms import AppConfig, Committee, CommitteeMembership, News, Staff, StaticPageContent
 from .Admin import Admin
-=======
->>>>>>> 0f8549f (small bug fix)
 from .base import Base
 from .Admin import Admin
 from .BudgetData import BudgetData
