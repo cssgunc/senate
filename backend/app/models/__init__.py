b89b3c359c49bb78ee1b421ecd891c593afd79af
Emily Jon <emilyljon156@gmail.com>
2026-02-24 08:14:11 -0500

    feat: add CMS backbone models (News, Committee, CommitteeMembership, Staff, StaticPageContent, AppConfig)

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,12 @@
 """SQLAlchemy models"""
+
+from app.models.cms import AppConfig, Committee, CommitteeMembership, News, Staff, StaticPageContent
+
+__all__ = [
+    "News",
+    "Committee",
+    "CommitteeMembership",
+    "Staff",
+    "StaticPageContent",
+    "AppConfig",
+]
